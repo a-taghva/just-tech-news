0f333d45ce1f65a38558866cadb06a38fc022eab
a-taghva <taghvali99@gmail.com>
2021-03-06 23:56:13 -0500

    exclude password from res.json

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -3,7 +3,9 @@ const { User } = require('../../models');
 
 router.get('/', (req, res) => {
     // Access our User model and run .findAll() method
-    User.findAll()
+    User.findAll({
+        attributes: { exclude: ['password'] }
+    })
         .then(dbUserData => res.json(dbUserData))
         .catch(err => {
             console.log(err);
@@ -13,6 +15,7 @@ router.get('/', (req, res) => {
 
 router.get('/:id', (req, res) => {
     User.findOne({
+        attributes: { exclude: ['password'] },
         where: {
             id: req.params.id
         }
