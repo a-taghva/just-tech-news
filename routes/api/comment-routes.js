e2e14bf1fcaf9f725ba56383f0cb563ef447f591
a-taghva <taghvali99@gmail.com>
2021-03-07 20:58:52 -0500

    create comment endpoints

---
+++ routes/api/comment-routes.js
@@ -0,0 +1,13 @@
+const router = require('express').Router();
+const { Comment } = require('../../models');
+
+router.get('/', (req, res) => {
+});
+
+router.post('/', (req, res) => {
+});
+
+router.delete('/:id', (req, res) => {
+});
+
+module.exports = router;
