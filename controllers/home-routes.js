d00a4aae4744275a44cc6cbdc71ecad467d2bb7f
a-taghva <taghvali99@gmail.com>
2021-03-19 09:53:13 -0400

    fix get single post

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -46,20 +46,45 @@ router.get('/login', (req, res) => {
     res.render('login');
 });
 
-router.get('post/:id', (req, res) => {
-    const post = {
-        id: 1,
-        post_url: 'https://handlebarsjs.com/guide/',
-        title: 'Handlebars Docs',
-        created_at: new Date(),
-        vote_count: 10,
-        comments: [{}, {}],
-        user: {
-            username: 'test_user'
-        }
-    };
+router.get('/post/:id', (req, res) => {
+    Post.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: [
+            'id',
+            'post_url',
+            'title',
+            'created_at',
+            [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+        .then(dbPostData => {
+            if (!dbPostData) {
+                return res.status(404).json({ message: 'No post found with this id' });
+            };
 
-    res.render('single-post', { post });
+            const post = dbPostData.get({ plain: true });
+            res.render('single-post', { post });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
 });
 
 module.exports = router;
